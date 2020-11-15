187a69d5953512177920f3d57d0f7f0e1b5ca314
Willfrans Varón Sáenz <willi@wixys.com>
2020-11-15 19:57:50 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,70 @@
+<template>
+  <div id="app">
+    <Button
+      :labelName="'Open link'"
+      :variant="'outline-success'"
+      :icon="'arrow-right-circle'"
+      :iconClass="'ml-3'"
+      :size="'lg'"
+      :link="'https://v3.vuejs.org/guide/instance.html'"
+      :pill="false"
+      :squared="true"
+    />
+    <Button
+      :labelName="'Say Hello'"
+      :variant="'primary'"
+      :icon="'code'"
+      :iconClass="'ml-2'"
+      :size="'md'"
+      :setFunction="sayHello"
+      :pill="false"
+      :squared="false"
+    />
+    <Button
+      :labelName="'Say bye'"
+      :variant="'danger'"
+      :icon="'code'"
+      :iconClass="'ml-2'"
+      :size="'sm'"
+      :setFunction="sayBye"
+      :pill="true"
+      :squared="false"
+    />
+    <Button
+      :labelName="'Do something'"
+      :variant="'warning'"
+      :icon="'arrow-right-circle'"
+      :iconClass="'ml-2'"
+      :size="'lg'"
+      :setFunction="doSomething"
+      :pill="false"
+      :squared="true"
+    />
+  </div>
+</template>
+
+<script>
+import Button from "./components/Button.vue";
+
+export default {
+  name: "App",
+  components: {
+    Button,
+  },
+  methods: {
+    // Add function to use with a button
+    sayHello() {
+      alert("Say hello!");
+    },
+    sayBye() {
+      alert("Say bye!");
+    },
+    doSomething() {
+      console.log("Do something...");
+    },
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ src/components/Button.vue
@@ -0,0 +1,75 @@
+<template>
+  <div>
+    <template v-if="link">
+      <b-link :href="link" target="_blank">
+        <b-button
+          :variant="variant"
+          :size="size"
+          class="m-3"
+          :pill="pill"
+          :squared="squared"
+        >
+          {{ labelName }}
+          <b-icon :icon="icon" :class="iconClass"></b-icon>
+        </b-button>
+      </b-link>
+    </template>
+    <template v-else-if="setFunction">
+      <b-button
+        :variant="variant"
+        :size="size"
+        class="m-3"
+        @click="setFunction"
+        :pill="pill"
+        :squared="squared"
+      >
+        {{ labelName }}
+        <b-icon :icon="icon" :class="iconClass"></b-icon>
+      </b-button>
+    </template>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Button",
+  props: {
+    labelName: {
+      type: String,
+      required: false,
+    },
+    variant: {
+      type: String,
+      required: false,
+    },
+    icon: {
+      type: String,
+      required: false,
+    },
+    iconClass: {
+      type: String,
+      required: false,
+    },
+    link: {
+      type: String,
+      required: false,
+    },
+    size: {
+      type: String,
+      required: false,
+    },
+    pill: {
+      type: Boolean,
+      required: false,
+    },
+    squared: {
+      type: Boolean,
+      required: false,
+    },
+    setFunction: {
+      type: Function,
+      required: false,
+    },
+  },
+};
+</script>
